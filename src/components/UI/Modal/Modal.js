61bb6085e54915e92ccc71be44e4b43973573e1b
Igor <munkieryder@gmail.com>
2020-05-08 18:16:57 +0200

    showing and hiding modal -  trasnsform  fix

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,8 +6,7 @@ const modal = props => (
     <div
         className={classes.Modal}
         style={{
-            transform: props.show ? 'translateY: (0)' : 'translateY: (-100vh)',
-            opacity: props.show ? '1' : '0'
+            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',            opacity: props.show ? '1' : '0'
         }}
     >
         {props.children}
